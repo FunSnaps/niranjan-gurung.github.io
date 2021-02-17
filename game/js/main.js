75d9ec456409db0b3f1a1b3a692f02bdf90ec08b
niranjan-gurung <nimagurung1@yahoo.com>
2021-02-17 12:42:39 +0000

    github repo name

--- game/js/main.js
+++ game/js/main.js
@@ -10,7 +10,7 @@
   if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
       navigator.serviceWorker
-        .register('../sw_cached_pages.js')
+        .register('https://niranjan-gurung.github.io/sw_cached_pages.js')
         .then(reg => console.log('Service Worker: Registered!'))
         .catch(err => console.log(`Service Worker: Error: ${err}`))
     })
