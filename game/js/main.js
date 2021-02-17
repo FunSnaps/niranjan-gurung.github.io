8c469241c5fcce6450c266faa18b4e428ee5706e
niranjan-gurung <nimagurung1@yahoo.com>
2021-02-17 12:49:34 +0000

    stuff

--- game/js/main.js
+++ game/js/main.js
@@ -10,7 +10,7 @@
   if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
       navigator.serviceWorker
-        .register('https://niranjan-gurung.github.io/sw_cached_pages.js', {scope: 'https://niranjan-gurung.github.io'})
+        .register('/{https://niranjan-gurung.github.io}/sw_cached_pages.js', {scope: '/{https://niranjan-gurung.github.io}/'})
         .then(reg => console.log('Service Worker: Registered!'))
         .catch(err => console.log(`Service Worker: Error: ${err}`))
     })
